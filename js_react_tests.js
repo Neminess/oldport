77697e6b92b45b7b9543b5d69d9496ff01529f49
Nem Drake <nem.drake@dragonia.com>
2021-10-11 15:24:31 +0300

    Tests

--- js_react_tests.js
+++ js_react_tests.js
@@ -1,17 +1,37 @@
 let profile_move=document.querySelector("#profile-picture")
-let head_line=document.querySelector("#welcome")
-
-
 profile_move.addEventListener("click",function(){
-      //head_line.style.color= "red";
-      profile_move.animate([
-        {transform:"translateY(0px)"},
-        {transform:"translateX(500px"}
-      ],
-      {duration:3000},
-      {iterations: 1})
+      //let styler=window.getComputedStyle(prof_cont); 
+   // profile_move.style.marginLeft= 1+"%";}
       
-    })
-//Below is an effort to get the initial position of the container as pixels
-let lefty=document.querySelector("#prof-pic-container");
-console.log(lefty.style.right)
+//https://javascript.info/js-animation
+let startTime = Date.now(); // remember start time
+
+let timer = setInterval(function() {
+  // how much time passed from the start?
+  let timePassed = Date.now() - startTime;
+
+  if (timePassed >= 3000) {
+    clearInterval(timer); // finish the animation after 2 seconds
+    return;
+  }
+
+  // draw the animation at the moment timePassed
+  draw(timePassed);
+
+}, 20);
+
+// as timePassed goes from 0 to 2000
+// left gets values from 0px to 400px
+function draw(timePassed) {
+  profile_move.style.left = timePassed / 30 + '%';}
+  
+})
+/*
+//function that runs until timer stops
+//function replaces left-value of pic from initial to 50% gradually
+
+  let timber=2500;
+  while(timber>0){
+    profile_move.style.marginleft=50+"%";
+    timber-200;
+  }*/
