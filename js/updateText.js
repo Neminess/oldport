21083324aeaf2b537eecbc56e9b77412ee46aefa
Nem Drake <nem.drake@dragonia.com>
2021-12-10 12:27:57 +0200

    Update updateText.js

--- js/updateText.js
+++ js/updateText.js
@@ -1,12 +1,13 @@
 let changling=document.querySelector("#textPlace");
-const existText=document.querySelector("#existingText");
 let buttoner=document.querySelector("#addText");
-let tekstia=document.querySelector("#textContainer")
+
+
 
 buttoner.addEventListener("click", function(){
-   let what=document.createElement("div");
+   let neDiv=document.createElement("div");
    const appendComment=document.createTextNode(changling.value);
-   what.appendChild(appendComment);
-   document.body.insertBefore(what,changling);
-})
+   neDiv.appendChild(appendComment);
+   document.body.insertBefore(neDiv,changling);
+   
+});
  
