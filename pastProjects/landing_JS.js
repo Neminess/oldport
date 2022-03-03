4d0d425ff8db2b15aa62c32d9dceb3920658aa80
Neminess <73821948+Neminess@users.noreply.github.com>
2022-03-03 11:17:01 +0200

    Revert "More practice-projects"

--- pastProjects/landing_JS.js
+++
@@ -1,6 +0,0 @@
-let todesc=document.querySelector("#to-description")
-
-todesc.addEventListener("click",function(){
-    let desc=document.querySelector("#description");
-    desc.style.marginTop="20%";
-});
