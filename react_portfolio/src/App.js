013a20b8bf39b7d57075cbafc58f29e3e9bd3d22
Nem Drake <nem.drake@dragonia.com>
2022-05-23 16:56:57 +0300

    Added react-base
    Added react-base-code, started moving information from static to dynamic

---
+++ react_portfolio/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import About from "./About";
+function App() {
+  return (
+    <div className="mainPage">
+      <header className="Noora's Portfolio">
+        <Navbar/>
+        <About/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
