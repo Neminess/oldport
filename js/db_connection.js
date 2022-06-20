3f94fe8cd1f8cd4d6414659a87a33665329753c6
Neminess <73821948+Neminess@users.noreply.github.com>
2022-06-20 15:52:28 +0300

    Update db_connection.js

--- js/db_connection.js
+++ js/db_connection.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://nemirana:Tr1gg3Reed@cluster0.ninqb.mongodb.net/cluster0?retryWrites=true&w=majority";
+const uri = "";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
