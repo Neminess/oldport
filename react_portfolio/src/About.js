8127e41869d36acb46042e155891366d379c4d56
Nem Drake <nem.drake@dragonia.com>
2022-07-07 19:31:46 +0300

    Deployed react app hopefully

--- react_portfolio/src/About.js
+++ react_portfolio/src/About.js
@@ -6,8 +6,14 @@ function About(){
         <section id="mainContent">
           <h2>About me</h2>
           <p>My name is Noora Hiltunen and I am a student at <a href="https://www.taitotalo.fi" target="_blank" rel="noopener noreferrer">Taitotalo.</a></p>
-          <p>I've been learning coding since 2019, so I do not have that much experience yet. I am in search of a job as a Junior Front End dev or Front End Trainee. See contact-page for my contact information.</p>
-     </section>);
+               
+               <article name="backgroundInfo">
+                    <h2>Short background information</h2>
+                    <p>I have been learning web-coding for about 3 years now, the last two in school. The first year I learned about html and CSS, and how to use them together. 
+                         In school, we were taught about JS, one course of PhP, Wordpress and more.</p>
+                    <p>My favourite techs to use are JS, React JS and SQL. I am also interested in Wordpress for the simplicity and fast deployment it enables.</p>
+               </article>
+          </section>);
 }
 console.log("About outside");
 export default About;
